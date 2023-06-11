9e5c1c167854698b525e012eb802cee7e7600521
Christopher Sanchez <christophersanchez@Christophers-MacBook-Pro.local>
2023-06-11 14:30:57 -0600

    fixing auth page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,29 +1,35 @@
-import { useState } from 'react';
-import { Box, Heading, Button, Center, Text } from '@chakra-ui/react';
-import SignUpForm from '../../components/SignUpForm/SignUpForm';
-import LoginForm from '../../components/LoginForm/LoginForm';
-
+import { useState } from "react";
+import { Box, Heading, Button, Center, Text } from "@chakra-ui/react";
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import LoginForm from "../../components/LoginForm/LoginForm";
 
 export default function AuthPage({ setUser }) {
   const [showSignUp, setShowSignUp] = useState(false);
 
   return (
     <Center height="100vh">
+      <Center>
+        <Image src="/CookConnect.jpeg" alt="CookConnect Image" mt={4} mb={8} />
+      </Center>
       <Box p={4} maxW="400px" borderWidth="1px" borderRadius="md">
         <Heading as="h1" size="xl" mb={4}>
-          {showSignUp ? 'Sign Up' : 'Login'}
+          {showSignUp ? "Sign Up" : "Login"}
         </Heading>
+        <Text mb={4}>
+          Welcome to CookConnect! Login to start sharing your amazing recipes
+          and to enjoy recipes others have shared.
+        </Text>
         {showSignUp ? (
           <SignUpForm setUser={setUser} />
         ) : (
           <LoginForm setUser={setUser} />
         )}
         <Center>
-          <Text mb={3}>Or</Text>
+          <Text mb={3}>Don't have an account?</Text>
         </Center>
         <Center>
           <Button onClick={() => setShowSignUp(!showSignUp)} mb={4} w="88%">
-            {showSignUp ? 'Already have an account? Log In' : 'Sign Up Here'}
+            {showSignUp ? "Already have an account? Log In" : "Sign Up Here"}
           </Button>
         </Center>
       </Box>
